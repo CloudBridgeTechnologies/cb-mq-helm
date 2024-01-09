66992f7f71510dd0986ddf75af7dcbfd56e65195
arvind-cb <112891099+arvind-cb@users.noreply.github.com>
2024-01-09 11:36:42 +0000

    Update install.sh

--- samples/AWSEKSPartnerSolution/deploy/install.sh
+++ samples/AWSEKSPartnerSolution/deploy/install.sh
@@ -23,12 +23,19 @@ if [ $# -gt 3 ]
     MQ_APP_PASSWORD_NAME="--set queueManager.envVariables[1].name=MQ_APP_PASSWORD"
     MQ_APP_PASSWORD_VALUE="--set queueManager.envVariables[1].value=${4}"
     APPLICATION_NAME="${2}"
+    persistence_datapvc_size="--set persistence.dataPVC.size=${9}"
+    persistence_logpvc_size="--set persistence.logPVC.size=${10}"
+    persistence_qmpvc_size="--set persistence.qmPVC.size=${11}"
+    resources_cpu_limit="--set resources.limits.cpu=${12}"
+    resources_memory_limit="--set resources.limits.memory=${13}"
+    resources_cpu_request="--set resources.requests.cpu=${14}"
+    resources_memory_request="--set resources.limits.memory=${15}"
 fi
 
 echo "...Queue Manager Application Name is $APPLICATION_NAME..."
 echo "...Namespace is $TARGET_NAMESPACE..."
 
-if [ $# -eq 8 ]
+if [ $# -eq 15 ]
   then
     LB_ANNOTATION="--set-string route.loadBalancer.annotations.service\.beta\.kubernetes\.io/aws-load-balancer-internal=${5}"
     LB_ANNOTATION="--set-string route.loadBalancer.annotations.service\.beta\.kubernetes\.io/aws-load-balancer-subnets=${6}\,${7}\,${8}"
@@ -43,4 +50,4 @@ export APP_CERT=$(cat ../../genericresources/createcerts/application.crt | base6
 kubectl config set-context --current --namespace=$TARGET_NAMESPACE
 kubectl apply -f mtlsqm.yaml
 
-helm install $APPLICATION_NAME ../../../charts/ibm-mq -f secureapp_nativeha.yaml $MQ_ADMIN_PASSWORD_NAME $MQ_ADMIN_PASSWORD_VALUE $MQ_APP_PASSWORD_NAME $MQ_APP_PASSWORD_VALUE $LB_ANNOTATION
+helm install $APPLICATION_NAME ../../../charts/ibm-mq -f secureapp_nativeha.yaml $MQ_ADMIN_PASSWORD_NAME $MQ_ADMIN_PASSWORD_VALUE $MQ_APP_PASSWORD_NAME $MQ_APP_PASSWORD_VALUE $LB_ANNOTATION $persistence_datapvc_size $persistence_logpvc_size $persistence__qmpvc_size $resources_cpu_request $resources_memory_request  
